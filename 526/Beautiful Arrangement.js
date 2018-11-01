213d43454274846b94ce998d86ac240ffb1dc9ab
Shuai Guo <shuai@ShuaideMBP.lan>
2018-11-01 23:49:59 +0800

    526:Beautiful Arrangement
    1.是暴力的用穷举取出来的
    2.于是参数大于10的时候会爆内存超过限制
    3.百度之后，回溯，正在构思

--- 526/Beautiful Arrangement.js
+++ 526/Beautiful Arrangement.js
@@ -4,27 +4,36 @@
  */
 var countArrangement = function(N) {
     var result = 0;
-    var a = [];
-    for (var i = 0;i<N;i++){
-        a.push(i+1);
+    var a = [[1]];
+    //每一列数
+    for (var i = 1;i<N;i++){
+        var b =[];
+        //前一列有多少项
+        for (var j = 0; j < a.length; j++) {
+            //每项不同位置插入
+            for (var y = 0; y < i+1; y++) {
+                var tmp = [];
+                Object.assign(tmp,a[j]);
+                tmp.splice(y, 0, i+1);
+                b.push(tmp);
+            }
+        }
+        a = b;
     }
-    var b = a;
-    for (var i = 0;i<N;i++){
-        for (var j = 0;j<N;j++){
-            a=b.slice(0);
-            swap(a,i,j);
-            console.log(a);
+    for (var i = 0; i < a.length; i++) {
+        if(isArrangement(a[i])){
+            result++;
         }
     }
-    return a;
+    return result;
 };
 
-var swap = function (array,a,b) {
-    var tmp = array[a];
-    array[a] = array[b];
-    array[b] = tmp;
-}
-console.log(countArrangement(3));
 
-//现在是，如何生成    所有序列，来遍历。
-//脑子，特乱。。。
+var isArrangement = function(arr){
+    for (var i = 0; i < arr.length; i++) {
+        if ((i+1) % arr[i] !== 0 && arr[i] % (i+1) !== 0) {
+            return false;
+        }
+    }
+    return true;
+}
