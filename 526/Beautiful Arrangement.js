784ca008445faf0609b885baa7c9b9d1b804bccd
Shuai Guo <shuai@ShuaideMBP.lan>
2018-11-03 14:10:53 +0800

    526:Beautiful Arrangement
    1.自己用回溯的办法实现了，但是徒增了老多数组复制
    2.看看人家的for循环，好精妙，到最后还给换回来

--- 526/Beautiful Arrangement.js
+++ 526/Beautiful Arrangement.js
@@ -2,38 +2,76 @@
  * @param {number} N
  * @return {number}
  */
-var countArrangement = function(N) {
+var countArrangement = function(N){
+    var a =[];
     var result = 0;
-    var a = [[1]];
-    //每一列数
-    for (var i = 1;i<N;i++){
-        var b =[];
-        //前一列有多少项
-        for (var j = 0; j < a.length; j++) {
-            //每项不同位置插入
-            for (var y = 0; y < i+1; y++) {
-                var tmp = [];
-                Object.assign(tmp,a[j]);
-                tmp.splice(y, 0, i+1);
-                b.push(tmp);
-            }
-        }
-        a = b;
+    var kao = 0;
+    //构造一个顺序数组
+    for (var i = 0;i<N;i++){
+        a.push(i+1);
     }
-    for (var i = 0; i < a.length; i++) {
-        if(isArrangement(a[i])){
+    var arrangement = function(arr,restArr,i){
+        if (arr.length==N) {
             result++;
+            console.log(arr);
+            return true;
+        }
+        if (i > restArr.length) {
+            return false;
+        }
+        var index = arr.length + 1;
+        // console.log(kao,arr,restArr,i,index);
+        kao++;
+        if (index % restArr[i] == 0 || restArr[i] % index == 0) {
+            arrangement(arr,restArr,i+1);
+            var tmparr = [];
+            var tmprestArr = [];     
+            Object.assign(tmparr,arr);
+            Object.assign(tmprestArr,restArr);
+            tmparr.push(restArr[i]);
+            tmprestArr.splice(i,1);
+            arrangement(tmparr,tmprestArr,0);
+        }else{
+            arrangement(arr,restArr,i+1);
+        }
+
+    }
+    arrangement([],a,0);
+    return result;
+}
+
+//别人的方法
+var countArrangement = function(N) {
+    const nums = new Array(N + 1).fill(1).map((item, idx) => idx);
+    return backtrack(nums, 1);
+};
+
+var backtrack = function(nums, begin) {
+    if (begin >= nums.length-1) {
+        if (isBeautiful(begin, nums[begin])) {
+            return 1;
+        }
+        return 0;
+    }
+    let result = 0;
+    for (let i = begin; i < nums.length; i++) {
+        if (isBeautiful(begin, nums[i])) {
+            swap(nums, i, begin);
+            result += backtrack(nums, begin+1);
+            swap(nums, i, begin);
         }
     }
     return result;
-};
-
-
-var isArrangement = function(arr){
-    for (var i = 0; i < arr.length; i++) {
-        if ((i+1) % arr[i] !== 0 && arr[i] % (i+1) !== 0) {
-            return false;
-        }
-    }
-    return true;
 }
+
+var isBeautiful = function(i, num) {
+    return (i % num === 0) || (num % i === 0); 
+}
+
+var swap = function(nums, i, j) {
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+console.log(countArrangement(3));
